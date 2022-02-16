40a3d6bf98cf940fd3f94ea1345a0616074a46d9
winterhi <73983805+winterhi@users.noreply.github.com>
2022-02-16 22:05:40 +0900

    JPA 테스트 코드

---
+++ src/test/java/com/jojoldu/book/springboot/domains/posts/PostsRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.jojoldu.book.springboot.domains.posts;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After // Junit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정.
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder() // posts 테이블에 insert/update 쿼리를 실행. id 값이 있다면 update, 없다면 insert 실행됨.
+                .title(title)
+                .content(content)
+                .author("winterhi@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll(); // posts 테이블의 모든 데이터를 조회하는 메소드.
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
