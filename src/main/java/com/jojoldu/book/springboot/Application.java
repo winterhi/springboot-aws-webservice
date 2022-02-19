787e58adc8529cd10442276c2fabc4fb30923e25
winterhi <73983805+winterhi@users.noreply.github.com>
2022-02-19 19:50:37 +0900

    JPA Auditing으로 생성 및 수정 시간 자동화

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,7 +2,9 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 어노테이션을 모두 활성화할 수 있도록 함.
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor // 기본 생성자 자동 추가
 @Entity // 테이블과 링크될 클래스임을 나타냄. 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭함. ex) SalesManager.java → sales_manager table
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id // 해당 테이블의 PK 필드를 나타냄.
     @GeneratedValue(strategy = GenerationType.IDENTITY) // PK의 생성 규칙. 스프링 부트 2.0에서는 GenerationType.IDENTITY 해줘야 auto-increment가 된다.
