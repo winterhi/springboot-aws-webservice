0230ac22c67d001d3682eec24ab4b3e53eb42e04
winterhi <73983805+winterhi@users.noreply.github.com>
2022-02-20 16:17:55 +0900

    게시글 등록 화면&기능

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,8 +1,10 @@
 package com.jojoldu.book.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
@@ -11,5 +13,11 @@ public class IndexController {
         /* 머스테치 스타터 덕분에 문자열 반환해도 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨.
         즉, src/main/resources/templates/index.mustache로 전환되어 View Resolver가 처리하게 됨. */
         return "index";
+    }
+
+    // /posts/save를 호출하면 posts-save.mustache를 호출하는 메소드
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
